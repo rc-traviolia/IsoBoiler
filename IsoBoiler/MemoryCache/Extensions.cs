86b2f8e02f90cea83ade81064a80fc15ef5be337
Richard Traviolia <richard.traviolia@dotfoods.com>
2025-05-26 11:45:13 -0500

    8.1.14 added MemoryCache.Extensions

---
+++ IsoBoiler/MemoryCache/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsoBoiler.MemoryCache
+{
+    public static class Extensions
+    {
+        public static async Task<TDataModel> Get<TDataModel>(this IMemoryCache memoryCache, string cacheKey, Func<Task<TDataModel>> refreshFunction, TimeSpan? timeToLive = null)
+        {
+            var cachedResults = memoryCache.Get<TDataModel>(cacheKey);
+
+            if (cachedResults == null)
+            {
+                var results = await refreshFunction();
+                memoryCache.Set(cacheKey, results, timeToLive ?? new TimeSpan(3, 0, 0)); //3 hr default
+                return results;
+            }
+            else
+            {
+                return cachedResults;
+            }
+        }
+
+        public static async Task<TDataModel> Get<TDataModel>(this IMemoryCache memoryCache, Func<Task<TDataModel>> refreshFunction, TimeSpan? timeToLive = null)
+        {
+            var cacheKey = !refreshFunction.Method.Name.Contains('<') ? refreshFunction.Method.Name : throw new Exception("If you want to pass an anonymous lambda function into this method you must provide a cacheKey value as well.");
+            var cachedResults = memoryCache.Get<TDataModel>(cacheKey);
+
+            if (cachedResults == null)
+            {
+                var results = await refreshFunction();
+                memoryCache.Set(cacheKey, results, timeToLive ?? new TimeSpan(3, 0, 0)); //3 hr default
+                return results;
+            }
+            else
+            {
+                return cachedResults;
+            }
+        }
+    }
+}
